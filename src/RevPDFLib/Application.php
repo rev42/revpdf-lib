f4a930e48dc9a5f637d03269664b4b5889e1157a
Olivier Cornu <olivier.cornu@revpdf.org>
2012-02-09 21:44:51 +0100

    sc devient dic

--- src/RevPDFLib/Application.php
+++ src/RevPDFLib/Application.php
@@ -42,45 +42,45 @@ class Application
     const NAME = 'RevPDFLib';
     const VERSION = '2.0.0 (20120129)';
     
-    private $sc = null;
+    private $dic = null;
     
     public function __construct()
     {
-        $this->sc = new DependencyInjection\ContainerBuilder();
-        $this->sc->register('exporter', 'RevPDFLib\Exporter\PdfExporter');
+        $this->dic = new DependencyInjection\ContainerBuilder();
+        $this->dic->register('exporter', 'RevPDFLib\Exporter\PdfExporter');
     }
     
     protected function selectDataProvider($value)
     {
-        $this->sc->register('provider', 'RevPDFLib\DataProvider\\' . $value);
+        $this->dic->register('provider', 'RevPDFLib\DataProvider\\' . $value);
     }
     
     public function export($data)
     {
         switch (gettype($data)) {
             case 'array':
-                $this->sc->register('reader', 'RevPDFLib\Reader\ArrayReader');
+                $this->dic->register('reader', 'RevPDFLib\Reader\ArrayReader');
                 break;
             case 'object':
                 if (get_class($data) == 'SimpleXMLElement') {
-                    $this->sc->register('reader', 'RevPDFLib\Reader\SimpleXMLReader');
+                    $this->dic->register('reader', 'RevPDFLib\Reader\SimpleXMLReader');
                 }
                 break;
             default:
                 throw new Exception();
         }
         // Get data properly formatted
-        $report = $this->sc->get('reader')->parseData($data);
+        $report = $this->dic->get('reader')->parseData($data);
         
         // Get data provider
         $this->selectDataProvider($report['source']['provider']);
-        $this->sc->get('provider')->parse($report['source']['value']);
-        $data = $this->sc->get('provider')->data;
+        $this->dic->get('provider')->parse($report['source']['value']);
+        $data = $this->dic->get('provider')->data;
         
         // Build document and generate it
         $document = null;
         if (is_array($report)) {
-            $document = $this->sc->get('exporter')->buildDocument($report, $data);
+            $document = $this->dic->get('exporter')->buildDocument($report, $data);
         }
         return $document;
     }
