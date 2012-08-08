0716d9d2887f1fb94b5d5cc2db1277bde3f05b23
Olivier Cornu <olivier.cornu@gmail.com>
2012-08-08 21:54:53 +0200

    refactor examples

--- example/pdo/inventory.php
+++ example/pdo/inventory.php
@@ -6,11 +6,10 @@ defined('BASE_DIR') or define('BASE_DIR', dirname(__file__) . '/../');
 
 require BASE_DIR . 'vendor/autoload.php';
 
-
-$conn = new PDO('mysql:dbname=revpdf;host=localhost', 'root', 'password');
+$conn = new PDO('mysql:dbname=revpdf;host=localhost', 'revpdf', 'password');
 $conn->exec("SET CHARACTER SET utf8");
 
 $lib = new RevPDFLib\Application();
-$data = simplexml_load_file('report-2.xml');
+$data = simplexml_load_file('inventory.xml');
 $lib->setDatasource($conn);
 $lib->export($data);
